fa05bf50c4d6caac680f3f99c42f168df0bef71b
RedClouud <jackchristie87@gmail.com>
2023-03-01 01:00:22 +0000

    refer to last commit

--- buildrun.bat
+++ buildrun.bat
@@ -3,12 +3,14 @@
 :: # This script is only usable on Windows 10 with Docker Desktop installed.
 
 :: Environment configuration
-set /A client_amount=5
+set /A client_amount=%1
 set client_name=flwr-client
-set server_name=flwr_server
+set server_name=flwr-server
 set network_name=flwr-network
 set client_image_name=flwr-client:1.0
 set server_image_name=flwr-server:1.0
+
+:: DO NOT CHANGE ANYTHING BELOW THIS LINE
 
 :: Rebuild the Docker image for server
 echo Rebuilding Docker image...
@@ -24,26 +26,24 @@ docker build -t %client_image_name% .
 ren Dockerfile Dockerfile-client 
 echo Docker image has been rebuilt.
 
-:: Deletes containers if they exist 
-docker rm flwr-server
-
-:: Deletes all containers if they exist
+:: Deletes exisitng containers 
+docker rm %server_name%
 for /L %%i in (1,1,%client_amount%) do docker rm %client_name%-%%i
 
-:: Creates containers and connects them to the fl_network
-docker create --name flwr-server --network flwr-network flwr-server:1.0
+:: Creates the network if it doesn't exist
+docker network create %network_name%
 
-:: Create all clients
-for /L %%i in (1,1,%client_amount%) do docker create --name %client_name%-%%i --network flwr-network flwr-client:1.0
+:: Creates containers and connects them to the fl_network
+docker create --name %server_name% --network %network_name% %server_name%:1.0
+for /L %%i in (1,1,%client_amount%) do docker create --name %client_name%-%%i --network %network_name% flwr-client:1.0
 
 echo All FL nodes have been created and connected to the fl_network.
 
+:: Deletes hanging (previous) image(s)
 echo Cleaning up...
 docker image prune --filter dangling=true -f
 
-:: Start the server
-docker start flwr-server
+:: Starts the containers
+docker start %server_name%
 timeout /t 5 >nul
-
-:: Start all clients
 for /L %%i in (1,1,%client_amount%) do docker start %client_name%-%%i
