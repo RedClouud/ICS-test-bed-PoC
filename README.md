6e7fbb9456969b2bdcd1c38f5ac396c3f9c40295
RedClouud <jackchristie87@gmai.com>
2023-03-01 22:58:30 +0000

    Improved run script

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
 # CPS testbed
 
 ## About
-Contains the resources required to simulate federeated learning implemented with docker... Enjoy!
 
-## buildrun.bat
-Intended for use on Windows
+Contains the resources required to simulate federeated learning implemented with docker.
+You will require wsl and docker desktop to run this example.
+
+Enjoy!
+
+## How to
+
+1. Execute run.sh
+2. Watch as your images are prepared and clients are started!
+
+Pro tip: use the flag -s (or --skip-build) to skip building!
+Pro'er tip: DON'T do this if you see an error :)
+
+### run.sh
+
 Creates docker containers for server and clients connected to a network
 
-.\build_run.bat [num of desired clients]
-
-## run.sh
-Intended for use on Linux
-Creates three terminals; one for a server and two for two clients
-
-bash run.sh
+bash run.sh [num of desired clients]
 
 ## Prerequisites
+
 - (nvidia-docker)[https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqazlKNzBtSk96dkdrWnNuLXNwdnBJRVFyWnRPZ3xBQ3Jtc0trMUwwMzFuYjNfWGRERUxjVVlXSTNRaWV4b2M1UlhXS1A0VjF6cUVtdnk3ZmlmYUpWdDFCZEVVdzh0d3Q2dkJIZW1sc3ZtMGxGdEg4MGdNQkQxNmtyamtOM3pIa1VtT3hzejFjanFCYzFpbEltX2szRQ&q=https%3A%2F%2Fgithub.com%2FNVIDIA%2Fnvidia-docker&v=-Y4T71UDcMY]
 
 ### Windows
+
 - Docker Desktop
 
 ### Linux
+
 - python3
-- flwr
-- torch
-- tqdm
--- buildrun.sh
+++ buildrun.sh
@@ -10,47 +10,56 @@ network_name=flwr-network
 client_image_name=flwr-client:1.0
 server_image_name=flwr-server:1.0
 
-# Rebuild the Docker image for server
-echo "Rebuilding server Docker image..."
-mv Dockerfile-server Dockerfile
-docker build -t $server_image_name .
-mv Dockerfile Dockerfile-server
-echo "Server Docker image has been rebuilt."
-
-# Rebuild the Docker image for client
-echo "Rebuilding client Docker image..."
-mv Dockerfile-client Dockerfile
-docker build -t $client_image_name .
-mv Dockerfile Dockerfile-client
-echo "Client Docker image has been rebuilt."
-
-# Deletes exisitng containers
-docker rm $server_name
-for i in $(seq 1 $client_amount)
-do
-docker rm "$client_name-$i"
-done
+# Download the CIFAR-10 dataset
+python3 -c "from torchvision.datasets import CIFAR10; CIFAR10('./data', download=True)"
 
 # Creates the network if it doesn't exist
 docker network create $network_name
+
+# Skip Docker image build if -s or --skip-build is passed as an argument
+if [[ $* != *-s* ]] && [[ $* != *--skip-build* ]]
+then
+
+    # Rebuild the Docker image for server
+    echo "Rebuilding server Docker image..."
+    mv Dockerfile-server Dockerfile
+    docker build -t $server_image_name .
+    mv Dockerfile Dockerfile-server
+    echo "Server Docker image has been rebuilt."
+
+    # Rebuild the Docker image for client
+    echo "Rebuilding client Docker image..."
+    mv Dockerfile-client Dockerfile
+    docker build -t $client_image_name .
+    mv Dockerfile Dockerfile-client
+    echo "Client Docker image has been rebuilt."
+
+    # Deletes hanging (previous) image(s)
+    echo "Cleaning up..."
+    docker image prune --filter dangling=true -f
+
+    # Deletes exisitng containers
+    docker rm $server_name
+    for i in $(seq 1 $client_amount)
+    do
+        docker rm "$client_name-$i"
+    done
+
+fi
 
 # Creates containers and connects them to the fl_network
 docker create --name $server_name --network $network_name $server_name:1.0
 for i in $(seq 1 $client_amount)
 do
-docker create --name "$client_name-$i" --network $network_name flwr-client:1.0
+    docker create --name "$client_name-$i" --network $network_name flwr-client:1.0
 done
 
 echo "All FL nodes have been created and connected to the fl_network."
-
-# Deletes hanging (previous) image(s)
-echo "Cleaning up..."
-docker image prune --filter dangling=true -f
 
 # Starts the containers
 docker start $server_name
 sleep 3
 for i in $(seq 1 $client_amount)
 do
-docker start "$client_name-$i"
+    docker start "$client_name-$i"
 done
